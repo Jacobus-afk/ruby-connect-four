bb75560234e6da7c73c19833451e30d8776e1af0
Jacobus-afk <s24162826@gmail.com>
2020-10-17 13:38:51 +0200

    figured out looping in rspec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,23 +4,32 @@ require './lib/game'
 require './lib/board'
 
 describe Game do
-  let(:board) { instance_double('Board') }
-  # before(:each) do
-  #   @game = Game.new
-  #   @game.instance_variable_set(:@board, instance_double(Board))
-  # end
+  # let(:board) { instance_double('Board') }
+  # let(:game) { Game.new }
+  before(:each) do
+    @game = Game.new
+    @game.instance_variable_set(:@board, instance_double(Board))
+  end
 
   describe '#play_round' do
-    before do
-      allow(Board).to receive(:new).and_return(board)
-    end
+    # before do
+    #   allow(Board).to receive(:new).and_return(board)
+    # end
     it 'prevents play from continuing until a valid move is made' do
       symbol = 'R'
       name = 'Test'
       # allow(@game).to receive(:_play_move).once.with(symbol, name)
-      allow(@game).to receive(:_play_move).and_return(false)
-      expect(@game).to receive(:puts)
-      @game.play_round(symbol, name)
+      allow(@game).to receive(:_play_move).and_return(false, true)
+      # (@game).to receive(:puts)
+      allow(@game).to receive(:_draw_board)
+      allow(@game).to receive(:_check_for_win)
+      allow(@game).to receive(:_check_for_full_board)
+
+      # play_move = double
+      # allow(@game).to receive(:_play_move).and_return(true)
+      # expect(@game).to receive(:puts)
+      expect { @game.play_round(symbol, name) }
+        .to output(/Invalid move. Retry/).to_stdout
     end
   end
 end
